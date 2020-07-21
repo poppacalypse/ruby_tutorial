327c5af6fdd9f58d9dee77cfb2ed32fb941afdb4
Carl Poppa <poppacalypse@gmail.com>
2020-07-21 20:28:48 +0800

    Add tutorial notes for .reduce, synonymous terminology, and Objects and classes (Defining classes)

--- functional.rb
+++ functional.rb
@@ -1,6 +1,6 @@
 states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"]
 
-# ========== .map ==========
+# ==================== .map ====================
 
 # Example 1 - urls: Imperative version
 # def imperative_urls states
@@ -26,11 +26,11 @@ states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"]
 
 # Example 3 - Returns a URL-friendly version of a string
 # e.g. "North Dakota" -> "north-dakota"
-# def urlify string
-#   string.downcase.split.join("-")
-# end
+def urlify string
+  string.downcase.split.join("-")
+end
 
-# Example 3A - urlified: Imperative version
+# Example 3A - urlify: Imperative version
 
 # def imperative_urls states
 #   urls = []
@@ -42,13 +42,27 @@ states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"]
 #
 # p imperative_urls states
 
-# Example 3B - urlified: Functional version
+# -----------------------------
+
+# Example 3B - urlify: Functional version
 
 # def functional_urls states
 #   states.map { |state| urlify(state) }
 # end
 #
 # p functional_urls states
+
+# -----------------------------
+
+# Example 3C - using synonymous .collect in place of .map
+
+# def functional_select_urls states
+#   states.collect { |state| urlify(state)}
+# end
+#
+# p functional_select_urls states
+
+# -----------------------------
 
 # .map Exercise: Using map, write a function that takes in the states variable and returns an array of URLs of the form https://example.com/<urlified form>.
 
@@ -73,42 +87,136 @@ states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"]
 # puts urlified_form2 states
 
 
-# ========== .select ==========
+# ==================== .select ====================
 
 # Example 1 - singles: Imperative version
-def imperative_singles states
-  singles = []
-  states.each do |state|
-    if state.split.length == 1
-      singles << state
-    end
-  end
-  singles
-end
+# def imperative_singles states
+#   singles = []
+#   states.each do |state|
+#     if state.split.length == 1
+#       singles << state
+#     end
+#   end
+#   singles
+# end
 
-# puts imperative_singles(states).inspect # parenthese necessary to use .inspect. Otherwise easier to just use p
+# puts imperative_singles(states).inspect # parentheses necessary to use .inspect. Otherwise easier to just use p
 
-p imperative_singles states
+# p imperative_singles states
 
 # -----------------------------
 
 # Example 2 - singles: Functional version
-# def functional_singles states
-#   states.select { |state| state.split.length == 1 }
-# end
-#
-# p functional_singles states
+def functional_singles states
+  states.select { |state| state.split.length == 1 }
+end
+
+p functional_singles states
+
+# -----------------------------
+
+# Example 3 - using antonymous .reject in place of .select (synonymous would be .find_all)
+
+def functional_find_all_singles states
+  states.find_all { |state| state.split.length == 1 }
+end
+
+p functional_find_all_singles states
+
+def functional_reject_singles states
+  states.reject { |state| state.split.length > 1 }
+end
+
+p functional_reject_singles states
+# -----------------------------
 
 # .select Exercise: Write two select functions that return the Dakotas: one using String#include? to test for the presence of the string “Dakota” and one that tests for the length of the split array being 2.
 
 # def has_dakota states
-#   states.select { |state| state.include?("Dakota") }
+#   states.select { |state| state.include? "Dakota" }
 # end
 #
 # p has_dakota states
 #
 # def two_word states
-#   states.select { |state| state.split.length == 2 } # or .length > 1
+#   states.select { |negeri| negeri.split.length == 2 } # or .length > 1
 # end
 #
 # p two_word states
+
+# ==================== .reduce ====================
+
+numbers = 1..10
+
+# Example 1 - sum: Imperative version
+# def imperative_sum numbers
+#   total = 0
+#   numbers.each do |n|
+#     total += n
+#   end
+#   total
+# end
+#
+# puts imperative_sum numbers
+
+# -----------------------------
+
+# Example 2 - sum: Functional version
+
+# def functional_sum numbers
+#   numbers.reduce { |total, n| total + n }
+# end
+#
+# puts functional_sum numbers
+
+# -----------------------------
+
+# Example 2A - using synonymous .inject in place of .reduce
+# def functional_inject_sum numbers
+#   numbers.inject { |total, n| total + n }
+# end
+#
+# puts functional_inject_sum numbers
+
+# -----------------------------
+
+# Example 3 - lengths: Imperative solution
+def imperative_lengths states
+  character_count = {}
+  states.each do |state|
+    character_count[state] = state.length
+  end
+  character_count
+end
+
+puts imperative_lengths states
+
+# -----------------------------
+
+# Example 4 - lengths: Functional solution
+# def functional_lengths states
+#   states.reduce({}) do |lengths, state|
+#     lengths[state] = state.length
+#     lengths
+#   end
+# end
+#
+# puts functional_lengths states
+
+# Example 4A - using .inject in place of .reduce
+# def functional_inject_lengths states
+#   states.inject({}) do |lengths, state|
+#     lengths[state] = state.length
+#     lengths
+#   end
+# end
+#
+# puts functional_inject_lengths states
+# -----------------------------
+
+# Example 5 - lengths: Functional, single-line solution
+# def compact_functional_lengths states
+#   states.reduce({}) { |lengths, state| lengths[state] = state.length; lengths }
+# end
+#
+# puts compact_functional_lengths states
--- palindrome.rb
+++ palindrome.rb
@@ -1,12 +1,42 @@
+# ==================== Functions and blocks ====================
+# ==================== Ch 5.3: Method chaining ====================
+
 # Returns true for a palindrome, false otherwise
 
-def palindrome? rentetan
-  processed_content = rentetan.downcase
-  processed_content == processed_content.reverse
-end
+# def palindrome? rentetan
+#   processed_content = rentetan.downcase
+#   processed_content == processed_content.reverse
+# end
 
 # Exercise: Using method chaining, write a function email_parts to return an array of the username and domain for a standard email address of the form username@example.com. Note: Make sure your function returns the same result for USERNAME@EXAMPLE.COM.
 
-def email_parts emel
-  emel.downcase.split("@")
+# def email_parts emel
+#   emel.downcase.split("@")
+# end
+
+# ==================== Objects and classes ====================
+# ==================== Ch 7.1: Defining classes ====================
+
+# Defines a Phrase class
+
+class Frasa
+  attr_accessor :kandungan # attribute accessor. Setter method that allows us to 'set' (assign) the given attribute
+
+  def initialize kontent
+    @kandungan = kontent # @kandungan = instance variable, which is available to use everywhere in the object
+  end
+
+  # Returns true for a palindrome, false otherwise (taken from 5.3)
+  def palindrome? # we don't need rentetan bcoz we already know what the string is
+    # processed_content = @kandungan.downcase
+    # processed_content = self.kandungan.downcase # more common way, where self refers to the @instance_variable, i.e. object instance. Similar to JS 'this'. It's not wrong to include it, and easier to have it at the beginning. Commonly used when we want to assign the kandungan. Otherwise, we can leave it off ↓
+    processed_content = kandungan.downcase
+    processed_content == processed_content.reverse
+  end
 end
+
+# phrase = Frasa.new "Madam, I'm Adam"
+# puts phrase.kandungan # attribute i.e. data. Getter method that allows us to 'get' (access) the given attribute.
+#
+# phrase.kandungan = "Able was I, ere I saw Elba."
+# puts phrase.kandungan
